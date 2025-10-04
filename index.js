4e5e5f7306c76fc7b9b83d6b3c36e97677d2156a
Dav <david.sanchez.martinez@outlook.com>
2025-10-04 22:24:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,3 +2,17 @@ import { menuArray } from './data.js'
 
 console.log(menuArray)
 
+const getMenuHtml = () => {
+    let menuHtml = ''
+
+    menuArray.forEach(menuItem => {
+        menuHtml += `
+            <div>
+        
+        
+        `
+
+    })
+
+
+}
