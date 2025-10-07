40a9c6c3c75160d4ccb30fe4f0e674fad04e1487
Dav <david.sanchez.martinez@outlook.com>
2025-10-07 22:42:52 +0100

    feat: finalize menu rendering feature

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 import { menuArray } from './data.js'
 
-console.log(menuArray)
-
-const getMenuHtml = () => {
+const generateMenuHtml = () => {
     let menuHtml = ''
-
+    
     menuArray.forEach(menuItem => {
         menuHtml += `
-            <div>
-        
-        
+            <div class="menu-item">
+                <img class="item-img" src="${menuItem.emoji}" alt="${menuItem.name}">
+                <div class="item-info">
+                    <p class="item-name">${menuItem.name}</p>
+                    <p class="item-ingredients">${menuItem.ingredients}</p>
+                    <p class="item-price">$${menuItem.price}</p>
+                </div>
+                <button class="add-btn">+</button>
+            </div>
         `
-
     })
 
-
+    return menuHtml
 }
+
+const renderMenu = () => {
+    document.getElementById('menu').innerHTML = generateMenuHtml() 
+}
+
+renderMenu()
